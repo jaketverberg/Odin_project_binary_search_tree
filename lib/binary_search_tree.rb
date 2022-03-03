d452f61973a399e56bc516574c1a354118a4c9ed
Leonerdos <54115094+jaketverberg@users.noreply.github.com>
2022-03-03 12:04:38 -0600

    Built scafolding outlined by Odin Project

---
+++ lib/binary_search_tree.rb
@@ -0,0 +1,61 @@
+class Node
+
+  def initialize(self, d)
+    self.data = d
+    self.left = none
+    self.right = none
+end
+
+
+class Tree
+attr_accessor: :root
+
+def build_tree(arr)
+  arr.sort
+  mid =
+end
+
+def insert
+
+end
+
+def delete
+
+end
+
+def find
+
+end
+
+def level_order
+
+end
+
+def inorder(&block)
+
+end
+
+def preorder(&block)
+
+end
+
+def postorder(&block)
+
+end
+
+def height(node)
+
+end
+
+def depth(node)
+
+end
+
+def balanced?
+
+end
+
+def rebalance
+
+end
+end
